175f3054b4afb0496606603bdf9992ca50261741
brenowillians <brenownmachado@gmail.com>
2024-02-08 11:28:00 -0300

    commit env

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,  { cors: true });
   app.useGlobalPipes(new ValidationPipe({forbidUnknownValues: false}));
 
-  await app.listen(7000);
+  await app.listen(7200);
 }
 bootstrap();
