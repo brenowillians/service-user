cd2a9ec25818ca8f22497cc97c3d6668b2eb1980
brenowillians <brenownmachado@gmail.com>
2024-02-22 20:53:45 -0300

    implementacao cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const port = process.env.API_PORT ? Number(process.env.API_PORT) : 7200;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,  { cors: true });
   app.useGlobalPipes(new ValidationPipe({forbidUnknownValues: false}));
 
   await app.listen(port);
